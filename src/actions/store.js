05628f8626cc35bb3760888471508ff93e24e11d
Alpha7 <henryomondi6612@gmail.com>
2023-10-21 17:52:12 +0300

    Make changes requested by the reviewer

--- src/actions/store.js
+++
@@ -1,8 +0,0 @@
-// src/store.js
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import greetingReducer from '../reducers/reducers';
-
-const store = createStore(greetingReducer, applyMiddleware(thunk));
-
-export default store;
